51609a8b7720029bfc98531369648a28b5780755
yaskovdev <yaskovdev@gmail.com>
2019-02-17 21:20:54 +0200

    Create empty iOS application.

---
+++ MathGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MathGame
+//
+//  Created by Sergey Yaskov on 17/02/2019.
+//  Copyright © 2019 Sergey Yaskov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
